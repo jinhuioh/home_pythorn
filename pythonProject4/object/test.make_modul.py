6e6b70b60422899036ce1518b81560cb5b5a430a
jinhuioh <aaa779979@naver.com>
2022-03-22 12:37:53 +0900

    e

---
+++ pythonProject4/object/test.make_modul.py
@@ -0,0 +1,38 @@
+# data_values= [{'title': '다이렉트<b>자동차</b>보험비교 견적사이트 / 특약 내용 정리'},
+#  {'title': '<b>자동차</b>보험다이렉트비교사이트 살펴보기'}]
+#
+# titles = []
+# descriptions = []
+# links = []
+#
+# def del_b(x):
+#     x = x.replace('</b>','')
+#     return x
+#
+# #title 키의 b태그 없애기
+# for i in range(0, len(data_values), 1):
+#     mdfy_title = (data_values[i]['title']).replace('<b>','')
+#     mdfy_title = del_b(mdfy_title)
+#     #mdfy_title = mdfy_title.replace('</b>','')
+#     #print(mdfy_title)
+#     titles.append(mdfy_title)
+# print("1번",titles)
+
+
+#문제:아래 코드의 replace('</b>','')를 함수로 모듈화 해보세요.
+data_values= [{'title': '다이렉트<b>자동차</b>보험비교 견적사이트 / 특약 내용 정리'},
+ {'title': '<b>자동차</b>보험다이렉트비교사이트 살펴보기'}]
+
+
+titles = []
+
+data_values[0]['title'] = data_values[0]['title'].replace('<b>','')
+result = data_values[0]['title'].replace('</b>', '')
+print(result)
+
+for i in range(0, len(data_values), 1):
+    data_values[i]['title'] = (data_values[i]['title']).replace('<b>','')
+    mdfy_title = (data_values[i]['title']).replace('</b>','')
+    titles.append(mdfy_title)
+
+print("1번",titles)
