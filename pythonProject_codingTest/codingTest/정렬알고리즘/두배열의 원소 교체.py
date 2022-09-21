e1ac32b21c763e632070880c3515379ea35d8be7
jinhuioh <aaa779979@naver.com>
2022-09-21 13:36:01 +0900

    codingTest:

--- pythonProject_codingTest/codingTest/정렬알고리즘/두배열의 원소 교체.py
+++ pythonProject_codingTest/codingTest/정렬알고리즘/두배열의 원소 교체.py
@@ -10,6 +10,21 @@
 # 세번째 줄에 배열 b의 원소들이 공백을 기준으로 구분되어 입력됨 모든 원소는 10,000,000보다 작은 자연수
 # 출력조건: 최대 k번의 바꿔치기 연산을 수행하여 만들 수 있는 배열 a의 모든 원소의 합이 최대값을 출력합니다.
 # ##
+
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a.sort()
+b.sort(reverse=True)
+print(a)
+print(b)
+for i in range(k):
+    if a[i] < b[i]:
+        a[i] = b[i]
+        print(a)
+print(sum(a))
+
+
 # n개의 원소와 바꿀 수 있는 수 k를 입력받자
 n,k = map(int, input().split())
 # n개만큼 배열들을 입력받자
