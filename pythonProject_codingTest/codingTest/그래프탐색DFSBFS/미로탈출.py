b28eb6fcd53d808d5ce3ac31bf2212dc57f2bc64
jinhuioh <aaa779979@naver.com>
2022-10-19 11:06:17 +0900

    codingTest

--- pythonProject_codingTest/codingTest/그래프탐색DFSBFS/미로탈출.py
+++ pythonProject_codingTest/codingTest/그래프탐색DFSBFS/미로탈출.py
@@ -11,44 +11,109 @@
 # 다음 n개의 줄에는 각각 m개의 정수(0또는 1)로 미로의 정보가 주어집니다.
 # 각각 수들은 공백 없이 입력으로 제시됩니다. 또한 시작 칸과 마지막 칸은 항상 1입니다.
 # 출력조건: 첫째 줄에 최소 이동 칸의 개수를 출력합니다.
-# ##
+
+
+#위치한 곳의 좌표가 1이면 이전꺼+좌표값(1)을 해서 카운트를 늘려감
+#범위를 벗어나거나, 0인경우 return false를 반환
 from collections import deque
-n,m = map(int,input().split())#세로가로입력
+
+# queue = deque([1,2])
+# one = queue.popleft()
+# one2 = queue.popleft()
+# print(one)
+# print(one2)
+
+
+n,m = map(int, input().split())
 graph = []
 for i in range(n):
     graph.append(list(map(int,input())))
-print(graph)
 
+
+
+#이동시킬 좌표값
 dx = [0,0,-1,1]
-dy = [-1,1,0,0]
-def bfs(x,y):#x는 가로 y는 세로
-    # 상하좌우에 대해 for문 돌리기
+dy = [1,-1,0,0]
+
+def miro(y,x):
     queue = deque()
-    queue.append((x,y))
+    queue.append((y,x))
+    print('큐~',queue)
     while queue:
-        print('queue',queue)
-        x,y = queue.popleft()
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+        yy,xx = queue.popleft()
+        print('yy',yy)
+        print('xx',xx)
+        #상하좌우로 채크해서 자리값이 1이면 카운트 증가시켜 이동시키기. 만약 자리값이 1이아니면 return False
+        for i in range(4):
+            nx = xx + dx[i]
+            ny = yy + dy[i]
+            if ny >= n or ny < 0 or nx >= m or nx < 0:
                 continue
-            if graph[ny][nx] == 0:
-                continue
+            print('ny nx', ny, nx)
             if graph[ny][nx] == 1:
-                graph[ny][nx] = graph[y][x] + 1
-                queue.append((nx,ny))
-    return graph[n-1][m-1]
-print(bfs(0,0))
+                print('1인 좌표로 이동')
+                graph[ny][nx] = graph[yy][xx] + 1
+                queue.append((ny, nx))
+                print('if문 안에queue append> ',queue)
+            else:
+                pass
+        print('queue append확인>> ',queue)
+    return print('답',graph[n-1][m-1])
+
+miro(0,0)
 
 
-# bfs(0,0)으로 시작해야함
 
 
 
 
 
 
+
+
+
+
+
+#
+#
+# # ##
+# from collections import deque
+# n,m = map(int,input().split())#세로가로입력
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int,input())))
+# print(graph)
+#
+# dx = [0,0,-1,1]
+# dy = [-1,1,0,0]
+# def bfs(x,y):#x는 가로 y는 세로
+#     # 상하좌우에 대해 for문 돌리기
+#     queue = deque()
+#     queue.append((x,y))
+#     while queue:
+#         print('queue',queue)
+#         x,y = queue.popleft()
+#         for k in range(4):
+#             nx = x + dx[k]
+#             ny = y + dy[k]
+#             if nx < 0 or nx >= m or ny < 0 or ny >= n:
+#                 continue
+#             if graph[ny][nx] == 0:
+#                 continue
+#             if graph[ny][nx] == 1:
+#                 graph[ny][nx] = graph[y][x] + 1
+#                 queue.append((nx,ny))
+#     return graph[n-1][m-1]
+# print(bfs(0,0))
+#
+#
+# # bfs(0,0)으로 시작해야함
+#
+#
+#
+#
+#
+#
 
 
 
--- pythonProject_codingTest/codingTest/그래프탐색DFSBFS/음료수 얼려 먹기.py
+++ pythonProject_codingTest/codingTest/그래프탐색DFSBFS/음료수 얼려 먹기.py
@@ -9,35 +9,77 @@
 # 출력조건
 # 한 번에 만들 수 있는 아이스크림 개수를 출력합니다.
 #
+
+n,m = map(int,input().split())#세로가로
+print(n,m)
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+print(graph)
+
+def ice(y,x):
+    # 범위를 벗어난다면
+    if x>=m or x<=-1 or y<=-1 or y>=n:
+        return False
+    if graph[y][x] == 0:
+        graph[y][x] = 1
+        print(graph)
+        ice(y+1,x)
+        ice(y-1,x)
+        ice(y,x+1)
+        ice(y,x-1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for k in range(m):
+        if ice(i,k) == True:
+            result += 1
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
 # 풀이방법: 특정 지점의 주변 상하좌우를 살펴본 뒤에 주변 지점 중에서 값이0이면서 아직 방문하지 않은 지점이 있다면 방문
 # 방문한 지점에서 다시 상하좌우를 살펴보면서 방문을 진행하는 과정을 반복하면 연결된 모든 지점을 방문할 수 있다.
 # 모든 노드에 대해 위에 방법 반복 그리고 지점의 수를 카운트
 # #
-
-n,m = map(int(input().split()))
-graph = []
-for i in range(n):
-    graph.append(list(map(int,input())))
-
-def ice(x,y):#x가 열(가로), y가 행(세로)
-    if x<0 or x>m or y<0 or y>n:
-        return False
-    if graph[y][x] == 0:
-        graph[y][x] == 1
-        ice(x+1,y)
-        ice(x-1,y)
-        ice(x,y+1)
-        ice(x,y-1)
-        return True
-    return False
-result =0
-for a in range(n):
-    for b in range(m):
-        if ice(a,b) == True:
-            result+=1
-
-print(result)
+#
+# n,m = map(int, input().split())
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int,input())))
+#
+# def ice(x,y):#x가 열(가로), y가 행(세로)
+#     if x<=-1 or x>=m or y<=-1 or y>=n:
+#         return False
+#     if graph[y][x] == 0:
+#         graph[y][x] = 1
+#         ice(x+1,y)
+#         ice(x-1,y)
+#         ice(x,y+1)
+#         ice(x,y-1)
+#         return True
+#     return False
+# result =0
+# for a in range(n):
+#     for b in range(m):
+#         if ice(a,b) == True:
+#             result+=1
+#
+# print(result)
 
 
 
@@ -85,25 +127,25 @@ for i in range(n):#행
 print(result)
 
 
-
-n,m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
-def ice(x,y):
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    if graph[x][y] == 0:
-        graph[x][y] == 1
-        ice(x-1, y)
-        ice(x, y-1)
-        ice(x+1, y)
-        ice(x, y+1)
-        return True
-    return False
-result = 0
-for i in range(n):
-    for j in range(m):
-        if ice(i,j) == True:
-            result +=1
-print(result)
+#
+# n,m = map(int, input().split())
+# graph = []
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+# def ice(x,y):
+#     if x <= -1 or x >= n or y <= -1 or y >= m:
+#         return False
+#     if graph[x][y] == 0:
+#         graph[x][y] == 1
+#         ice(x-1, y)
+#         ice(x, y-1)
+#         ice(x+1, y)
+#         ice(x, y+1)
+#         return True
+#     return False
+# result = 0
+# for i in range(n):
+#     for j in range(m):
+#         if ice(i,j) == True:
+#             result +=1
+# print(result)
