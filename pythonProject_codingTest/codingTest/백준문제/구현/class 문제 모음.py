991cc94029961708312f72f50d4eb8c562413093
jinhuioh <aaa779979@naver.com>
2022-11-01 10:59:32 +0900

    codingTest

--- pythonProject_codingTest/codingTest/백준문제/구현/class 문제 모음.py
+++ pythonProject_codingTest/codingTest/백준문제/구현/class 문제 모음.py
@@ -729,50 +729,60 @@ from collections import deque
 #
 # N+2번째 줄부터 M개의 줄에 걸쳐 비밀번호를 찾으려는 사이트 주소가 한줄에 하나씩 입력된다. 이때, 반드시 이미 저장된 사이트 주소가 입력된다.
 # <code>---------------------------------------------------------------------
-from sys import stdin
-n,m = map(int, stdin.readline().split())
-N = {}
-for i in range(n):
-    a,b = map(str, stdin.readline().split())
-    N[a] = b
-# print(N)
-#
-for i in range(m):
-    print(N[stdin.readline().rstrip()])
-
-
-
-
-
-
 # from sys import stdin
-# n,m = map(int, input().split())
-# graph1 = []
-# graph2 = []
+# n,m = map(int, stdin.readline().split())
+# # 딕셔너리 자료형 이용
+# N = {}
 # for i in range(n):
-#     graph1.append((stdin.readline().split()))
-# # queue1 = deque(graph1)
-#     # graph1.add(stdin.readline().rstrip())
+#     a,b = map(str, stdin.readline().split())
+#     #a를 key로 value를 b로 넣어준다.
+#     N[a] = b
+# # print(N)
+# #
 # for i in range(m):
-#     graph2.append(stdin.readline().rstrip())
-# print(graph1)
-# print('graph2',graph2)
+#     #a에 해당하는 b값을 출력
+#     print(N[stdin.readline().rstrip()])
 #
-# for one2 in graph2:
-#     for one1 in graph1:
-#         a,b = one1
-#         if one2 == a:
-#             print(b)
+
+
+
+# new============================================================================================================
+# 입력
+# 첫째 줄에는 도감에 수록되어 있는 포켓몬의 개수 N이랑 내가 맞춰야 하는 문제의 개수 M이 주어져.
+# N과 M은 1보다 크거나 같고, 100,000보다 작거나 같은 자연수인데, 자연수가 뭔지는 알지?
+# 모르면 물어봐도 괜찮아. 나는 언제든지 질문에 답해줄 준비가 되어있어.
+#
+# 둘째 줄부터 N개의 줄에 포켓몬의 번호가 1번인 포켓몬부터 N번에 해당하는 포켓몬까지 한 줄에 하나씩 입력으로 들어와.
+# 포켓몬의 이름은 모두 영어로만 이루어져있고, 또, 음... 첫 글자만 대문자이고, 나머지 문자는 소문자로만 이루어져 있어.
+# 아참! 일부 포켓몬은 마지막 문자만 대문자일 수도 있어. 포켓몬 이름의 최대 길이는 20, 최소 길이는 2야.
+# 그 다음 줄부터 총 M개의 줄에 내가 맞춰야하는 문제가 입력으로 들어와. 문제가 알파벳으로만 들어오면 포켓몬 번호를 말해야 하고,
+# 숫자로만 들어오면, 포켓몬 번호에 해당하는 문자를 출력해야해. 입력으로 들어오는 숫자는 반드시 1보다 크거나 같고,
+# N보다 작거나 같고, 입력으로 들어오는 문자는 반드시 도감에 있는 포켓몬의 이름만 주어져. 그럼 화이팅!!!
+#
+# 출력
+# 첫째 줄부터 차례대로 M개의 줄에 각각의 문제에 대한 답을 말해줬으면 좋겠어!!!.
+# 입력으로 숫자가 들어왔다면 그 숫자에 해당하는 포켓몬의 이름을, 문자가 들어왔으면 그 포켓몬의 이름에 해당하는 번호를 출력하면 돼.
+# 그럼 땡큐~
+# <code>---------------------------------------------------------------------
 # from sys import stdin
-# input = stdin.readline
+# n,m = map(int, stdin.readline().split())
+# N = {}
+# N2 = {}
+# for i in range(1,n+1):
+#     name = stdin.readline().rstrip()
+#     # 입력한 단어가 문자인 경우에 쓸 딕셔너리 key, value값
+#     N[name] = i
+#     # 입력한 단어가 숫자인 경우에 쓸 딕셔너리 key, value값
+#     N2[i] = name
+# # print(N)
 #
-# N, M = map(int, input().split())
-# result = {}
-#
-# for _ in range(N):
-#     site, ps = input().split()
-#     result[site] = ps
-# # print(result)
-#
-# for _ in range(M):
-#     print(result[input().rstrip()])
+# for i in range(m):
+#     # 입력받기
+#     one = stdin.readline().rstrip()
+#     # 들어온값이 알파벳이라면
+#     if one.isalpha() == True:
+#         print(N[one])
+#     else:
+#         print(N2[int(one)])
+
+
